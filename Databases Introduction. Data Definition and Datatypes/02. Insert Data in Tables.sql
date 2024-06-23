6b5483e1a8a2e5b0c66615026ff42a027011da5d
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-06-23 15:45:02 +0300

    new

---
+++ Databases Introduction. Data Definition and Datatypes/02. Insert Data in Tables.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees(`first_name`, `last_name`) 
+VALUES ("Field_First_Name_One", "Field_Second_Name_One"),
+		("Field_First_Name_Two", "Field_Second_Name_Two"),
+		("Field_First_Name_Three", "Field_Second_Name_Three");
