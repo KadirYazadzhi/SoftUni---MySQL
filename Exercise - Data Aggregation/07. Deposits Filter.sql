a1a5c4c52a1e55fe1fba5c710fbd7b5dbf4ebe82
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-07-04 14:37:39 +0300

    new

--- Exercise - Data Aggregation/07. Deposits Filter.sql
+++ Exercise - Data Aggregation/07. Deposits Filter.sql
@@ -2,4 +2,5 @@ SELECT `deposit_group`, SUM(`deposit_amount`) AS 'total_sum'
 FROM `wizzard_deposits`
 WHERE `magic_wand_creator` = "Ollivander family"
 GROUP BY `deposit_group`
-ORDER BY `deposit_group` ASC;
+HAVING `total_sum` < 150000
+ORDER BY `total_sum` DESC;
