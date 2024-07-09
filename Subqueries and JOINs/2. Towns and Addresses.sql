916db3da140856ddbb7577e3fc84f8c8005a6e50
kadir7563483 <160729048+KadirYazadzhi@users.noreply.github.com>
2024-07-10 01:53:19 +0300

    new

---
+++ Subqueries and JOINs/2. Towns and Addresses.sql
@@ -0,0 +1,6 @@
+SELECT a.`town_id`, t.`name` AS 'town_name', a.`address_text`  
+FROM `addresses` AS `a`
+JOIN `towns` AS `t` 
+ON a.`town_id` = t.`town_id`
+WHERE t.`name` = "San Francisco" OR t.`name` = "Sofia" OR t.`name` = "Carnation"
+ORDER BY a.`town_id`, a.`address_id`;
